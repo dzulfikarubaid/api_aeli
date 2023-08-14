5a89c64214ed76be8b36ffad203ab4265836d196
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-08-14 21:28:21 +0700

    initial commit

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ class Dpd(models.Model):
     nama = models.CharField(max_length=30, unique=True)
     alamat = models.TextField()
     contact = models.TextField()
+    email = models.TextField()
     gmaps = models.TextField(null=True, blank=True, default='')
 
     def __str__(self):
